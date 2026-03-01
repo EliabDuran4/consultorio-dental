d5aab929fc37c8e22e5f8acfadb32b2de42896ea
EliabDuran4 <supereliab54@gmail.com>
2026-03-01 12:45:51 -0600

    Estructura BD correcta y puerto Render

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ app.post('/citas', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
